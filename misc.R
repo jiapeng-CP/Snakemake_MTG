9baac5994d79d9d8805d581221c0a03394b2e6d2
jiapeng-CP <131789717+jiapeng-CP@users.noreply.github.com>
2023-11-08 12:57:07 -0500

    Create misc.R
    added Pavian

---
+++ misc.R
@@ -0,0 +1,6 @@
+# visualize taxon abundance with sankey plot by using Pavian https://github.com/fbreitwieser/pavian
+
+if (!require(remotes)) { install.packages("remotes") }
+remotes::install_github("fbreitwieser/pavian")
+
+shiny::runGitHub("fbreitwieser/pavian", subdir = "inst/shinyapp")
