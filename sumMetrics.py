246fd2e416bd1a1a8e0e5da297aa43408156abcc
jiapeng <jiapengc@alustt132.cm.cluster>
2024-01-30 15:18:53 -0600

    replace sumFastP.py with sumMetrics.py. Added metrics clean_reads, mapped2HOMD and mapped_perc

---
+++ sumMetrics.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import json
+
+metaDF = pd.read_csv("samplesheet.tsv", sep = "\t", index_col="Sample")
+
+df = pd.DataFrame(columns=["Sample", "raw_reads", "clean_reads", "mapped2HOMD", "mapped_perc"])
+for s in metaDF.index:
+
+    # read fastp json
+    fpjs = "fastp/" + s + ".json"
+    f = open(fpjs)
+    fpd = json.load(f)
+    raw_reads = fpd['summary']['before_filtering']['total_reads']
+
+    # read map2HOMD json
+    hojs = "map2HOMD/" + s + ".json"
+    f = open(hojs)
+    hopd = json.load(f)
+    clean_reads = hopd['general']['reads']['total'] # reads used for eHOMD mapping are trimmed
+    mapped2HOMD = hopd['general']['reads']['mapped']['0']
+
+    mapped_perc = mapped2HOMD * 100 / clean_reads
+
+
+    mlist = [s,
+              str(raw_reads),
+              str(clean_reads),
+              str(mapped2HOMD),
+              str(mapped_perc)
+              ]
+
+    df = df.append(pd.Series(mlist, index=df.columns), ignore_index=True)
+
+
+
+
+df.to_csv("metrics.tsv", sep='\t', index=False)
