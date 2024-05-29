be0b694c1da6238c720a02838c6a2f1270b18e87
jiapeng <jiapengc@alustt132.cm.cluster>
2024-05-29 12:50:27 -0500

    added map2human metrics

--- sumMetrics.py
+++ sumMetrics.py
@@ -3,30 +3,40 @@ import json
 
 metaDF = pd.read_csv("samplesheet.tsv", sep = "\t", index_col="Sample")
 
-df = pd.DataFrame(columns=["Sample", "raw_reads", "clean_reads", "mapped2HOMD", "mapped_perc"])
+df = pd.DataFrame(columns=["Sample", "raw_reads", "clean_reads", "mapped2HOMD", "HOMD_perc"])
 for s in metaDF.index:
 
     # read fastp json
     fpjs = "fastp/" + s + ".json"
-    f = open(fpjs)
-    fpd = json.load(f)
+    with open(fpjs, 'r') as f:
+        fpd = json.load(f)
     raw_reads = fpd['summary']['before_filtering']['total_reads']
 
     # read map2HOMD json
     hojs = "map2HOMD/" + s + ".json"
-    f = open(hojs)
-    hopd = json.load(f)
+    with open(hojs) as f:
+        hopd = json.load(f)
     clean_reads = hopd['general']['reads']['total'] # reads used for eHOMD mapping are trimmed
     mapped2HOMD = hopd['general']['reads']['mapped']['0']
 
-    mapped_perc = mapped2HOMD * 100 / clean_reads
+    HOMD_perc = mapped2HOMD * 100 / clean_reads
+
+    # read map2human json
+    hujs = "map2human/" + s + ".bamstat.json"
+    with open(hujs) as f:
+        hupd = json.load(f)
+    mapped2Human = hupd['general']['reads']['mapped']['0']
+
+    Human_perc = mapped2Human * 100 / clean_reads
 
 
     mlist = [s,
               str(raw_reads),
               str(clean_reads),
               str(mapped2HOMD),
-              str(mapped_perc)
+              str(HOMD_perc),
+              str(mapped2Human),
+              str(Human_perc)
               ]
 
     df = df.append(pd.Series(mlist, index=df.columns), ignore_index=True)
